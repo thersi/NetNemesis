d5ab2e60f1a99cb5a8f1d7288a7e35a78e51b9f4
Toobaias <Toobaias@gmail.com>
2023-02-15 23:02:11 +0100

    Funker nesten

--- Arduino/Robotic_arm_Braccio/Robotic_arm_Braccio/Robotic_arm_Braccio.ino
+++ Arduino/Robotic_arm_Braccio/Robotic_arm_Braccio/Robotic_arm_Braccio.ino
@@ -9,6 +9,20 @@
 #include <Braccio.h>
 
 String line;
+char *c_servo1;
+char *c_servo2;
+char *c_servo3;
+char *c_servo4;
+char *c_servo5;
+char *c_servo6;
+
+String servo1="";
+String servo2="";
+String servo3="";
+String servo4="";
+String servo5="";
+String servo6="";
+
 
 Servo base;
 Servo shoulder;
@@ -86,35 +100,75 @@ void setup() {
 }
 
 void loop() {
-  readSerial();
-  realPos();
+  //readSerial();
+  psuedoReadSerial();
+  //realPos();
   updateServos();
-  writeSerial();
+  //writeSerial();
 }
 
 void readSerial() {
   while (Serial.available() > 0) {
     line = Serial.readString();
-    if (line.indexOf("Servo") > 0) {
-      servo2Pos = line.substring(line.indexOf("Servo2: "), line.indexOf(";")).toInt();
-      servo1Pos = line.substring(line.indexOf("Servo1: "), line.indexOf(";")).toInt();
-      servo3Pos = line.substring(line.indexOf("Servo3: "), line.indexOf(";")).toInt();
-      servo4Pos = line.substring(line.indexOf("Servo4: "), line.indexOf(";")).toInt();
-      servo5Pos = line.substring(line.indexOf("Servo5: "), line.indexOf(";")).toInt();
-      servo6Pos = line.substring(line.indexOf("Servo6: "), line.indexOf(";")).toInt();
+    if (line.indexOf("ervo") > 0) {
+      servo1Pos = line.substring(line.indexOf("Servo1: ") + 8, line.indexOf("Servo1: ") + 10).toInt();
+      servo2Pos = line.substring(line.indexOf("Servo2: ") + 8, line.indexOf("Servo2: ") + 10).toInt();
+      servo3Pos = line.substring(line.indexOf("Servo3: ") + 8, line.indexOf("Servo3: ") + 10).toInt();
+      servo4Pos = line.substring(line.indexOf("Servo4: ") + 8, line.indexOf("Servo4: ") + 10).toInt();
+      servo5Pos = line.substring(line.indexOf("Servo5: ") + 8, line.indexOf("Servo5: ") + 10).toInt();
+      servo6Pos = line.substring(line.indexOf("Servo6: ") + 8, line.indexOf("Servo6: ") + 10).toInt();
 
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(500);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(500);
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(500);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(500);
     }
   }
 }
 
+void psuedoReadSerial() {
+    line = "Servo1: 50; Servo2: 50; Servo3: 50; Servo4: 50; Servo5: 50; Servo6: 50;";
+    if (line.indexOf("ervo") > 0) {
+      c_servo1 = strtok(line.c_str(), ";");
+      c_servo2 = strtok(NULL, ";");
+      c_servo3 = strtok(NULL, ";");
+      c_servo4 = strtok(NULL, ";");
+      c_servo5 = strtok(NULL, ";");
+      c_servo6 = strtok(NULL, ";");
+
+      String serov1 = String(c_servo1);
+      String servo2 = String(c_servo2);
+      String servo3 = String(c_servo3);
+      String servo4 = String(c_servo4);
+      String servo5 = String(c_servo5);
+      String servo6 = String(c_servo6);
+
+      servo1Pos = servo1.substring(servo1.lastIndexOf(": "), servo1.length() - 1).toInt();
+      servo2Pos = servo2.substring(servo2.lastIndexOf(": "), servo2.length() - 1).toInt();
+      servo3Pos = servo3.substring(servo3.lastIndexOf(": "), servo3.length() - 1).toInt();
+      servo4Pos = servo4.substring(servo4.lastIndexOf(": "), servo4.length() - 1).toInt();
+      servo5Pos = servo5.substring(servo5.lastIndexOf(": "), servo5.length() - 1).toInt();
+      servo6Pos = servo6.substring(servo6.lastIndexOf(": "), servo6.length() - 1).toInt();
+
+    Serial.println(servo1);
+    //Serial.println(servo2);
+    //Serial.println(servo3);
+    //Serial.println(servo4);
+    //Serial.println(servo5);
+    //Serial.println(servo6);
+
+    //Serial.println(servo1Pos);
+    //Serial.println(servo2Pos);
+    //Serial.println(servo3Pos);
+    //Serial.println(servo4Pos);
+    //Serial.println(servo5Pos);
+    //Serial.println(servo6Pos);
+    }
+}
+
+
+void updateServos() {
+
+  Braccio.ServoMovement(10, servo1Pos, servo2Pos, servo3Pos, servo4Pos, servo5Pos, servo6Pos);
+  
+  delay(100);
+}
 
 void realPos() {
   servo1In = analogRead(A0);
@@ -151,9 +205,3 @@ void writeSerial() {
   }
 }
 
-void updateServos() {
-
-  Braccio.ServoMovement(10, servo1Pos, servo2Pos, servo3Pos, servo4Pos, servo5Pos, servo6Pos);
-  
-  delay(50);
-}
--- gui/controller.py
+++ gui/controller.py
@@ -10,8 +10,8 @@ class Controller:
         ser = serial.Serial('COM4', 9600, timeout=1)
 
         form.onButton.clicked.connect(lambda: ser.write(str.encode(
-            'Servo1: 50; Servo2: 50; Servo3: 50; Servo4: 50; Servo5: 50; Servo6: 50;')))
+            'Servo1: 80; Servo2: 80; Servo3: 80; Servo4: 80; Servo5: 80; Servo6: 80;')))
         form.offButton.clicked.connect(
-            lambda: ser.write(str.encode('Servo1: 20; Servo2: 20; Servo3: 20; Servo4: 20; Servo5: 20; Servo6: 20;')))
+            lambda: ser.write(str.encode('Servo1: 0; Servo2: 0; Servo3: 0; Servo4: 0; Servo5: 0; Servo6: 0;')))
 
 # 'Servo1: 200; Servo2: 200; Servo3: 200; Servo4: 200; Servo5: 200; Servo6: 200;'
