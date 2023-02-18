4b8cc1b54acfbf5e3d7d794345c73b5064cadfc0
Toobaias <Toobaias@gmail.com>
2023-02-18 01:32:51 +0100

    Comport finding

--- gui/controller.py
+++ gui/controller.py
@@ -1,13 +1,26 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QDialog
 import serial
+import serial.tools.list_ports
 import time
 
+ports = serial.tools.list_ports.comports()
 
 class Controller:
     def __init__(self, form):
         self.form = form
-        ser = serial.Serial('COM4', 9600, timeout=1)
+
+        if (len(ports) == 0):
+            print("No serial ports found")
+            exit()
+        
+        for port in ports:
+            if ("Arduino Uno" in port.description):
+                print("Arduino Uno found")
+                comport = port.device
+
+        
+        ser = serial.Serial(comport, 9600, timeout=1)
 
         form.onButton.clicked.connect(lambda: ser.write(str.encode(
             'Servo1: 90; Servo2: 45; Servo3: 180; Servo4: 180; Servo5: 0; Servo6: 30;')))
