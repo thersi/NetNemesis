f5d8b9aa42264058d52d09b668f61af726c630d0
Therese <therese.sigmundstad@gmail.com>
2023-03-01 09:40:56 +0100

    Add gui test simulator

--- gui/model.py
+++ gui/model.py
@@ -2,9 +2,8 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QDialog
 from PyQt6.QtCore import QUrl
 from controller import controller
-from gui.arm_sim.robot_toolbox import robot_simulator
+from robot_sim import robot_simulator
 from xbox_controller import XboxController
-
 
 robot_simulator()
 Form, Window = uic.loadUiType("gui/view.ui")
