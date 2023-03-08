f5333c8be4094dda526180c9662aae6ed378e2f6
Toobaias <Toobaias@gmail.com>
2023-03-08 08:45:19 +0100

    Arduino working good

--- Arduino/Robotic_arm_Braccio/Robotic_arm_Braccio/Robotic_arm_Braccio.ino
+++ Arduino/Robotic_arm_Braccio/Robotic_arm_Braccio/Robotic_arm_Braccio.ino
@@ -88,7 +88,7 @@ int c;
 
 float m, n;
 //Serial greier
-const byte numChars = 128;
+const byte numChars = 32;
 char receivedChars[numChars];
 char tempChars[numChars];
 
@@ -99,13 +99,14 @@ float floatFromPC = 0.0;
 bool newData = false;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   //myservo.attach(3);
   //Serial.setTimeout(100);
   
   pinMode(12, OUTPUT);    //you need to set HIGH the pin 12
   digitalWrite(12, HIGH);
   Braccio.begin(SOFT_START_DISABLED);
+  updateServos();  
   
   pinMode(LED_BUILTIN, OUTPUT);
   
@@ -121,28 +122,24 @@ void setup() {
 }
 
 void loop() {
-  //readSerial();
-  readSerialNB();
+  readSerial();
   if (newData == true) {
         digitalWrite(LED_BUILTIN, HIGH);
         strcpy(tempChars, receivedChars);
           // this temporary copy is necessary to protect the original data
           //   because strtok() used in parseData() replaces the commas with \0
         parseData();
-        //showParsedData();
+        updateServos();
         newData = false;
     }
-  
-  //psuedoReadSerial();
   realPos();
-  updateServos();
+  //updateServos();
   writeSerial();
 }
 void parseData() {      // split the data into its parts
 
     char * strtokIndx; // this is used by strtok() as an index
 
-    
     strtokIndx = strtok(tempChars,",");      // get the first part - the string
     servo1Pos = atoi(strtokIndx);     // convert this part to an integer    
  
@@ -160,67 +157,6 @@ void parseData() {      // split the data into its parts
 
     strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
     servo6Pos = atoi(strtokIndx);     // convert this part to an integer   
-/*
-    strtokIndx = strtok(tempChars,":");      // get the first part - the string
-    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
-    if (messageFromPC == "Servo1") {
-      strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-      servo1Pos = atoi(strtokIndx);     // convert this part to an integer
-    }
-    else if (messageFromPC == "Servo2") {
-      strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-      servo2Pos = atoi(strtokIndx);     // convert this part to an integer      
-    }
-    else if (messageFromPC == "Servo3") {
-      strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-      servo3Pos = atoi(strtokIndx);     // convert this part to an integer      
-    }
-    else if (messageFromPC == "Servo4") {
-      strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-      servo4Pos = atoi(strtokIndx);     // convert this part to an integer      
-    }
-    else if (messageFromPC == "Servo5") {
-      strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-      servo5Pos = atoi(strtokIndx);     // convert this part to an integer      
-    }
-    else if (messageFromPC == "Servo6") {
-      strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-      servo6Pos = atoi(strtokIndx);     // convert this part to an integer      
-    }
-    strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-    servo1Pos = atoi(strtokIndx);     // convert this part to an integer
-    
-    strtokIndx = strtok(NULL,":");      // get the first part - the string
-    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
- 
-    strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-    servo2Pos = atoi(strtokIndx);     // convert this part to an integer
-
-    strtokIndx = strtok(NULL,":");      // get the first part - the string
-    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
- 
-    strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-    servo3Pos = atoi(strtokIndx);     // convert this part to an integer
-
-    strtokIndx = strtok(NULL,":");      // get the first part - the string
-    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
- 
-    strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-    servo4Pos = atoi(strtokIndx);     // convert this part to an integer
-
-    strtokIndx = strtok(NULL,":");      // get the first part - the string
-    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
- 
-    strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-    servo5Pos = atoi(strtokIndx);     // convert this part to an integer
-
-    strtokIndx = strtok(NULL,":");      // get the first part - the string
-    strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
- 
-    strtokIndx = strtok(NULL, ";"); // this continues where the previous call left off
-    servo6Pos = atoi(strtokIndx);     // convert this part to an integer
-
-    */
     
     Serial.println(servo1Pos);
     Serial.println(servo2Pos);
@@ -232,7 +168,7 @@ void parseData() {      // split the data into its parts
 }
 
 
-void readSerialNB() {
+void readSerial() {
   static boolean recvInProgress = false;
   static byte ndx = 0;
   char startMarker = '<';
@@ -266,125 +202,10 @@ void readSerialNB() {
 
 }
 
-void readSerial() {
-  if (Serial.available() > 0) {
-    line = Serial.readString();
-    digitalWrite(LED_BUILTIN, HIGH);
-    if (line.indexOf("ervo1") > 0 && line.length() > 70) {
-      c_servo1 = strtok(line.c_str(), ";");
-      c_servo2 = strtok(NULL, ";");
-      c_servo3 = strtok(NULL, ";");
-      c_servo4 = strtok(NULL, ";");
-      c_servo5 = strtok(NULL, ";");
-      c_servo6 = strtok(NULL, ";");
-
-      servo1 = String(c_servo1);
-      servo2 = String(c_servo2);
-      servo3 = String(c_servo3);
-      servo4 = String(c_servo4);
-      servo5 = String(c_servo5);
-      servo6 = String(c_servo6);
-
-      servo1Pos = servo1.substring(servo1.indexOf(":") + 2, servo1.length()).toInt();
-      servo2Pos = servo2.substring(servo2.indexOf(":") + 2, servo2.length()).toInt();
-      servo3Pos = servo3.substring(servo3.indexOf(":") + 2, servo3.length()).toInt();
-      servo4Pos = servo4.substring(servo4.indexOf(":") + 2, servo4.length()).toInt();
-      servo5Pos = servo5.substring(servo5.indexOf(":") + 2, servo5.length()).toInt();
-      servo6Pos = servo6.substring(servo6.indexOf(":") + 2, servo6.length()).toInt();
-      
-    }
-    else if (line.indexOf("ervo1") > 0 && line.length() < 17) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println("one ");
-    }
-    else if (line.indexOf("ervo2") > 0 && line.length() < 17) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println("two ");
-    }
-    else if (line.indexOf("ervo3") > 0 && line.length() < 17) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println("three ");
-    }
-    else if (line.indexOf("ervo4") > 0 && line.length() < 17) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println("four ");
-    }
-    else if (line.indexOf("ervo5") > 0 && line.length() < 17) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println("five ");
-    }
-    else if (line.indexOf("ervo6") > 0 && line.length() < 17) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println("six ");
-    }
-  }
-  digitalWrite(LED_BUILTIN, LOW);
-}
-
-void psuedoReadSerial() {
-    //line = "Servo1: 90; Servo2: 45; Servo3: 180; Servo4: 180; Servo5: 90; Servo6: 10;";
-    line = "Servo3: 180";
-    if (line.indexOf("ervo1") > 0 && line.length() > 70) {
-      c_servo1 = strtok(line.c_str(), ";");
-      c_servo2 = strtok(NULL, ";");
-      c_servo3 = strtok(NULL, ";");
-      c_servo4 = strtok(NULL, ";");
-      c_servo5 = strtok(NULL, ";");
-      c_servo6 = strtok(NULL, ";");
-
-      servo1 = String(c_servo1);
-      servo2 = String(c_servo2);
-      servo3 = String(c_servo3);
-      servo4 = String(c_servo4);
-      servo5 = String(c_servo5);
-      servo6 = String(c_servo6);
-
-      servo1Pos = servo1.substring(servo1.indexOf(":") + 2, servo1.length()).toInt();
-      servo2Pos = servo2.substring(servo2.indexOf(":") + 2, servo2.length()).toInt();
-      servo3Pos = servo3.substring(servo3.indexOf(":") + 2, servo3.length()).toInt();
-      servo4Pos = servo4.substring(servo4.indexOf(":") + 2, servo4.length()).toInt();
-      servo5Pos = servo5.substring(servo5.indexOf(":") + 2, servo5.length()).toInt();
-      servo6Pos = servo6.substring(servo6.indexOf(":") + 2, servo6.length()).toInt();
-
-    Serial.println(servo1Pos);
-    Serial.println(servo2Pos);
-    Serial.println(servo3Pos);
-    Serial.println(servo4Pos);
-    Serial.println(servo5Pos);
-    Serial.println(servo6Pos);
-    }
-
-    else if (line.indexOf("ervo1") > 0 && line.length() < 13) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println(servo1Pos);
-    }
-    else if (line.indexOf("ervo2") > 0 && line.length() < 13) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println(servo2Pos);
-    }
-    else if (line.indexOf("ervo3") > 0 && line.length() < 13) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println(servo3Pos);
-    }
-    else if (line.indexOf("ervo4") > 0 && line.length() < 13) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println(servo4Pos);
-    }
-    else if (line.indexOf("ervo5") > 0 && line.length() < 13) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println(servo5Pos);
-    }
-    else if (line.indexOf("ervo6") > 0 && line.length() < 13) {
-      servo1Pos = line.substring(line.indexOf(":") + 2, line.length()).toInt();
-      Serial.println(servo6Pos);
-    }
-}
-
-
 void updateServos() {
 
   if (oldServo6Pos != servo6Pos) {
-    Braccio.ServoMovement(10, 90, 90, 90, 40, 45, servo6Pos);
+    Braccio.ServoMovement(0, 90, 90, 90, 40, 45, servo6Pos);
     oldServo6Pos = servo6Pos;    
   }
 
@@ -460,4 +281,3 @@ void writeSerial() {
     //break;
   }
 }
-
