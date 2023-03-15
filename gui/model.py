84b96ef61613d8fbb5472b8ccf76ead34afee2c4
Therese <therese.sigmundstad@gmail.com>
2023-03-15 13:42:01 +0100

    Add robot to gui

--- gui/model.py
+++ gui/model.py
@@ -1,7 +1,20 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
 from controller import Controller
-from PyQt6.QtCore import QUrl
+from arm_sim.pyplot_example import SimulationWidget
+from matplotlib.figure import Figure
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+from arm_sim.init_robot import EiT_arm
+
+import numpy as np
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import roboticstoolbox as rtb
+import threading
+import time
+
+from arm_sim.init_robot import EiT_arm
 
 
 Form, Window = uic.loadUiType("gui/view.ui")
@@ -9,9 +22,45 @@ app = QApplication([])
 window = Window()
 form = Form()
 form.setupUi(window)
-form.simulationView.load(QUrl('http://localhost:52000/?53084'))
-form.simulationView.show()
+
+# Create figure with subplots
+figure = Figure()
+ax1 = figure.add_subplot(121, projection="3d")
+
+canvas = FigureCanvas(figure)
+
+layout = form.simulationLayout
+layout.addWidget(canvas)
+
+
+arm = EiT_arm()
+
+
+dt = 0.05
+
+
+def q_change():
+    while True:
+        arm.q += 0.3*np.random.random(size=arm.q.shape)
+        time.sleep(dt)
+
+
+t = threading.Thread(target=q_change, daemon=True)
+t.start()
+
+
+env = rtb.backends.PyPlot.PyPlot()
+
+env.launch("environment")
+
+env.ax = ax1
+env.add(arm)
+
+
 Controller(form)
 window.showMaximized()
-# window.show()
 app.exec()
+
+while True:
+    env.step(dt)
+    canvas.draw()
