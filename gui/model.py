8b1286035953d7a99a9ad0cca5278ff83e203a3d
Therese <therese.sigmundstad@gmail.com>
2023-03-15 14:50:04 +0100

    Add controller to model

--- gui/model.py
+++ gui/model.py
@@ -1,6 +1,6 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
-# from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import QTimer
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from arm_sim.init_robot import EiT_arm
@@ -38,32 +38,33 @@ ctr.start(dt)
 arm.q = [0.21, -0.03, 0.35, -1.90, -0.04]
 
 
+env = rtb.backends.PyPlot.PyPlot()
+
+
 def q_change():
+    flag = False
     while True:
         # get encoder values. Here simulated perfectly (no noise)
         arm.q = arm.q + dt*(arm.qd)
-        env.step(dt)  # update plot
-        canvas.draw()
         if ctr.arrived:
             if flag:
                 ctr.set_pos(arm.fkine([1, 0, 1, 0, 1]).A)
             else:
                 ctr.set_pos(arm.fkine([0.61, -0.03, 0.35, -1.90, -0.04]).A)
             flag = not flag
+        time.sleep(dt)
 
 
 t = threading.Thread(target=q_change, daemon=True)
 t.start()
 
 
-env = rtb.backends.PyPlot.PyPlot()
-
 env.launch("environment")
 
 env.ax = ax1
 env.add(arm)
 
-""" 
+
 def update():
     env.step(dt)
     canvas.draw()
@@ -72,7 +73,7 @@ def update():
 # Initialize QTimer
 timer = QTimer()
 timer.timeout.connect(update)
-timer.start(1) """
+timer.start(1)
 
 
 # Controller(form)
