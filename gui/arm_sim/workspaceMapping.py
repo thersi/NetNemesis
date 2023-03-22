c859f40aa1772ae5bb7de2be50b6dbe098d8204f
Henrik Hammarstrøm <henrhamm@stud.ntnu.no>
2023-03-22 13:49:55 +0100

    Added progress bar.

--- gui/arm_sim/workspaceMapping.py
+++ gui/arm_sim/workspaceMapping.py
@@ -2,6 +2,7 @@ from init_robot import EiT_arm
 import matplotlib.pyplot as plt
 import numpy as np
 import itertools
+from tqdm import tqdm
 
 def main():
     arm = EiT_arm()
@@ -9,9 +10,14 @@ def main():
     angle_array = np.linspace(0, 270, 10) * np.pi / 180
 
     angles = itertools.product(angle_array, repeat=5)
-    poses = [arm.fkine(a) for a in angles]
+    num_poses = len(angle_array) ** 5
 
-    coords = np.array([p.t for p in poses])
+    coords = np.zeros((num_poses, 3))
+    with tqdm(total=num_poses) as pbar:
+        for i, a in enumerate(angles):
+            pose = arm.fkine(a)
+            coords[i] = pose.t
+            pbar.update(1)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
