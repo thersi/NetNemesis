c6ad3cf22e25454691e4d387dde1714b795ef13f
Henrik Hammarstrøm <henrhamm@stud.ntnu.no>
2023-03-22 15:28:38 +0100

    Added more contrast to contour plot.

--- gui/arm_sim/WorkspaceContour.py
+++ gui/arm_sim/WorkspaceContour.py
@@ -7,14 +7,14 @@ from tqdm import tqdm
 def main():
     arm = EiT_arm()
 
-    angle_array = np.linspace(0, 270, 10) * np.pi / 180
+    angle_array = np.linspace(0, 270, 15) * np.pi / 180
 
     angles = itertools.product(angle_array, repeat=5)
     num_poses = len(angle_array) ** 5
 
     x_coords = []
     y_coords = []
-    #coords = np.zeros((num_poses, 3))
+    
     with tqdm(total=num_poses) as pbar:
         for i, a in enumerate(angles):
             pose = arm.fkine(a)
@@ -24,13 +24,17 @@ def main():
             pbar.update(1)
 
     # Create 2D histogram
-    H, xedges, yedges = np.histogram2d(x_coords, y_coords, bins=50)
+    H, xedges, yedges = np.histogram2d(x_coords, y_coords, bins=100)
     extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    
+    # Set vmin and vmax to increase contrast of areas with no measurements
+    vmin = np.percentile(H, 10)
+    vmax = np.percentile(H, 90)
     
     # Create heatmap
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    im = ax.imshow(H.T, extent=extent, origin='lower', cmap='hot')
+    im = ax.imshow(H.T, extent=extent, origin='lower', cmap='hot', vmin=vmin, vmax=vmax)
     cbar = plt.colorbar(im)
     cbar.set_label('Density')
     ax.set_xlabel('X')
