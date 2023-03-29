5ee3226c942eec4f0d917dc288d7405950cd7380
Erling Hjermstad <erling-hjermstad@hotmail.com>
2023-03-29 11:48:17 +0200

    sliders affect arm_sim

--- gui/model.py
+++ gui/model.py
@@ -20,71 +20,6 @@ window = Window()
 form = Form()
 form.setupUi(window)
 
-
-def initialize_view():
-    # Set up angle slider 1
-    form.q1_slider.setMinimum(-135)
-    form.q1_slider.setMaximum(135)
-    form.q1_slider.setValue(0)
-    form.q1.setText(str(form.q1_slider.value()))
-    form.q1_slider.valueChanged.connect(
-        lambda: form.q1.setText(str(form.q1_slider.value())))
-
-    # Set up angle slider 2
-    form.q2_slider.setMinimum(-135)
-    form.q2_slider.setMaximum(135)
-    form.q2_slider.setValue(0)
-    form.q2.setText(str(form.q2_slider.value()))
-    form.q2_slider.valueChanged.connect(
-        lambda: form.q2.setText(str(form.q1_slider.value())))
-
-    # Set up angle slider 1
-    form.q1_slider.setMinimum(-135)
-    form.q1_slider.setMaximum(135)
-    form.q1_slider.setValue(0)
-    form.q1.setText(str(form.q1_slider.value()))
-    form.q1_slider.valueChanged.connect(
-        lambda: form.q1.setText(str(form.q1_slider.value())))
-
-    # Set up angle slider 2
-    form.q2_slider.setMinimum(-135)
-    form.q2_slider.setMaximum(135)
-    form.q2_slider.setValue(0)
-    form.q2.setText(str(form.q2_slider.value()))
-    form.q2_slider.valueChanged.connect(
-        lambda: form.q2.setText(str(form.q2_slider.value())))
-
-    # Set up angle slider 3
-    form.q3_slider.setMinimum(-135)
-    form.q3_slider.setMaximum(135)
-    form.q3_slider.setValue(0)
-    form.q3.setText(str(form.q3_slider.value()))
-    form.q3_slider.valueChanged.connect(
-        lambda: form.q3.setText(str(form.q3_slider.value())))
-
-    # Set up angle slider 4
-    form.q4_slider.setMinimum(-135)
-    form.q4_slider.setMaximum(135)
-    form.q4_slider.setValue(0)
-    form.q4.setText(str(form.q4_slider.value()))
-    form.q4_slider.valueChanged.connect(
-        lambda: form.q4.setText(str(form.q4_slider.value())))
-
-    # Set up angle slider 5
-    form.q5_slider.setMinimum(-135)
-    form.q5_slider.setMaximum(135)
-    form.q5_slider.setValue(0)
-    form.q5.setText(str(form.q5_slider.value()))
-    form.q5_slider.valueChanged.connect(
-        lambda: form.q5.setText(str(form.q5_slider.value())))
-
-    # Add modes to mode_select
-    form.mode_select.addItems(["Auto", "Position", "Optimization"])
-    form.mode_select.setCurrentIndex(0)
-
-
-
-initialize_view()
 # Create figure with subplots
 figure = plt.figure()
 form.simulationLayout.addWidget(figure.canvas)
@@ -93,6 +28,62 @@ arm = EiT_arm(q0=[0.21, -0.03, 0.35, -1.90, -0.04])  # initial pose
 # controller time steps, how often new qd is calculated and plots updated
 dt = 0.1
 
+def set_sliders():
+    ang = arm.q_degrees().astype(int)
+    form.q1_slider.setValue(ang[0])
+    form.q2_slider.setValue(ang[1])
+    form.q3_slider.setValue(ang[2])
+    form.q4_slider.setValue(ang[3])
+    form.q5_slider.setValue(ang[4])
+
+def slider_change():
+    arr = [form.q1_slider.value(),
+          form.q2_slider.value(),
+          form.q3_slider.value(),
+          form.q4_slider.value(),
+          form.q5_slider.value()]
+    
+    qs = np.asarray(arr)*np.pi/180
+    
+    form.q1.setText(str(arr[0]))
+    form.q2.setText(str(arr[1]))
+    form.q3.setText(str(arr[2]))
+    form.q4.setText(str(arr[3]))
+    form.q5.setText(str(arr[4]))
+    arm.q = qs
+
+def initialize_view():
+    # Set up angle slider 1
+    form.q1_slider.setMinimum(-135)
+    form.q1_slider.setMaximum(135)
+    form.q1_slider.valueChanged.connect(slider_change)
+
+    # Set up angle slider 2
+    form.q2_slider.setMinimum(-135)
+    form.q2_slider.setMaximum(135)
+    form.q2_slider.valueChanged.connect(slider_change)
+
+    # Set up angle slider 3
+    form.q3_slider.setMinimum(-135)
+    form.q3_slider.setMaximum(135)
+    form.q3_slider.valueChanged.connect(slider_change)
+
+    # Set up angle slider 4
+    form.q4_slider.setMinimum(-135)
+    form.q4_slider.setMaximum(135)
+    form.q4_slider.valueChanged.connect(slider_change)
+
+    # Set up angle slider 5
+    form.q5_slider.setMinimum(-135)
+    form.q5_slider.setMaximum(135)
+    form.q5_slider.valueChanged.connect(slider_change)
+
+    set_sliders()
+    # Add modes to mode_select
+    form.mode_select.addItems(["Auto", "Position", "Optimization"])
+    form.mode_select.setCurrentIndex(0)
+
+initialize_view()
 
 env = rtb.backends.PyPlot.PyPlot()
 env.launch(name="EiT environment", fig=figure)  # lauches a second plot
@@ -132,6 +123,7 @@ def changeTab(tabIndex):
     if tabIndex == 0:
         ctr.disable()
         ep.disable()
+        set_sliders()        
     elif tabIndex == 1:
         ep.set_pos(arm.fkine(arm.q).A)
         ctr.set_position(ep.get_pos())
